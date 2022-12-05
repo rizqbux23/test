d675865813597ed62b060cd00b996e0eaf794764
Rizqullah Bux <rizq_bux@etikas.net>
2022-12-05 21:51:22 +0800

    first commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$("li.dropdown").each(function () {
+    if ($("ul.dropdown-menu", this).children("li.nav-item").length == 0) {
+        $(this).hide();
+    }
+});
